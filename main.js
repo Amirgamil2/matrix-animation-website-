a340c344a2ce230c05f8f355e44daabe56bca485
Amir <amirgamil143@gmail.com>
2023-02-24 01:11:05 +0200

    creat my project

---
+++ main.js
@@ -0,0 +1,36 @@
+let stars = document.getElementById('stars')
+let moon = document.getElementById('moon')
+let mountains3 = document.getElementById('mountains3')
+let mountains4 = document.getElementById('mountains4')
+let river5 = document.getElementById('river')
+let boat6 = document.getElementById('boat')
+let Matrix = document.getElementById('Matrix')
+window.onscroll=function(){
+    let value = scrollY;
+    stars.style.left=value + 'px';
+    moon.style.top= value * 3.5 + 'px';
+    mountains3.style.top= value * 2 + 'px';
+    mountains4.style.top= value * 1.5 + 'px';
+    river5.style.left=value + 'px';
+    river5.style.top= value + 'px';
+    boat6.style.top= value + 'px';
+    boat6.style.left= value * 3  + 'px';
+    Matrix.style.fontSize= value  + 'px';
+    if(scrollY >= 92) {
+        Matrix.style.fontSize=92 + 'px';
+        Matrix.style.position= 'fixed';
+        if(scrollY >= 478){
+            Matrix.style.display='none';
+        }
+        else{
+            Matrix.style.display='block';
+        }
+        if(scrollY >= 127){
+            document.querySelector('.main').style.background = 'linear-gradient(#376281,#10001f)';
+            
+        }
+        else{
+            document.querySelector('.main').style.background = 'linear-gradient(#200016 , #10001f)';
+        }
+    }
+}
